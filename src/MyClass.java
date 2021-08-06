37766eba645e784671cf458149b12c536944f2ca
wijit.siripun@gmail.com <wijit.siripun@gmail.com>
2021-08-06 01:15:32 -0500

    Second Commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Wijit Myclass");
+		System.out.println("Hi Wijit Myclass Number 2");
 
 	}
 
